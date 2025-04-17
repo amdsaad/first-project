3efdc5779d1e92e34580de6c7f27cc6a6a938560
amdsaad <saadamd@gmail.com>
2025-04-17 16:52:38 +0200

    inital

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import TheTodos from './components/TheTodos.vue'
+</script>
+
+<template>
+  <main class="container">
+    <h1 class="text-center">TODO APP</h1>
+    <TheTodos />
+  </main>
+</template>
+
+<style scoped></style>
---
+++ src/components/TheTodos.vue
@@ -0,0 +1,56 @@
+<script setup>
+import { reactive, ref } from 'vue'
+
+const todoText = ref('')
+const todos = reactive([
+  {
+    id: 1,
+    text: 'buy milk',
+    done: false,
+  },
+  {
+    id: 2,
+    text: 'buy bread',
+    done: true,
+  },
+])
+
+const addTodo = () => {
+  // if not todo text return
+  if (!todoText.value.trim()) return
+  // add text to todos
+  todos.push({
+    id: todos.length + 1,
+    text: todoText.value,
+    done: false,
+  })
+
+  todoText.value = ''
+}
+</script>
+<template>
+  <div>
+    <form @submit.prevent="addTodo">
+      <div class="input-group">
+        <input
+          type="text"
+          placeholder="add todo"
+          v-model="todoText" />
+        <button type="submit">add</button>
+      </div>
+    </form>
+    <ul>
+      <li
+        class="todo-item"
+        v-for="todo in todos"
+        :key="todo.id">
+        <p>{{ todo.text }}</p>
+        <input
+          type="checkbox"
+          v-model="todo.done" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style></style>
