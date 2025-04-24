450a6f2e885e51f35ca4820e2927931ded51a00f
amdsaad <saadamd@gmail.com>
2025-04-24 17:06:41 +0200

    code update

--- src/App.vue
+++ src/App.vue
@@ -1,96 +1,115 @@
-<script lang="ts" setup>
-import { computed, ref } from 'vue'
+<script setup>
+import { ref, watch } from 'vue'
 
-// local components
-// import AppBtn from './components/AppBtn.vue'
+const name = ref('ahmed saad')
+const descripton = ref('Lorem ipsum dolor sit amet.  ')
+const agree = ref(true)
 
-const books = ref([
-  {
-    id: 1,
-    title: 'The Pragmatic Programmer',
-    author: 'Andrew Hunt',
-    genre: 'Programming',
-    published: 1999,
-    read: true,
-  },
-  {
-    id: 2,
-    title: 'Clean Code',
-    author: 'Robert C. Martin',
-    genre: 'Programming',
-    published: 2008,
-    read: true,
-  },
-  {
-    id: 3,
-    title: 'Atomic Habits',
-    author: 'James Clear',
-    genre: 'Self-help',
-    published: 2018,
-    read: true,
-  },
-  {
-    id: 4,
-    title: 'Deep Work',
-    author: 'Cal Newport',
-    genre: 'Productivity',
-    published: 2016,
-    read: false,
-  },
-  {
-    id: 5,
-    title: 'Refactoring',
-    author: 'Martin Fowler',
-    genre: 'Programming',
-    published: 1999,
-    read: false,
-  },
-])
+const teamMembers = ref([])
+const picked = ref('one')
+const selected = ref('')
+const age = ref(0)
 
-const redbooks = computed(() => {
-  return books.value.filter((x) => x.read).length
-})
-const markAsRead = (book) => {
-  // if (book.read) return
-  book.read = !book.read
-}
-
-const search = ref('')
-
-const filteredBooks = computed(() => {
-  return books.value.filter(book =>
-    book.title.toLowerCase().includes(search.value.toLowerCase())
-  )
-  // some changes
-
-  // another
-})
-
+watch(name, () => {})
 </script>
 
 <template>
   <div class="max-w-5xl mx-auto px-4 py-8">
-    <p class="text-center">i read {{ redbooks }} books</p>
-    <input type="text" v-model="search" placeholder="Search Book You Want">
-    <ul class="space-y-4 grid grid-cols-2 gap-4">
-      <li class="border p-4 rounded-md border-gray-200 shadow-sm" v-for="book in filteredBooks"
-        @click="markAsRead(book)" :key="book.id">
-        <h1 class="font-medium text-lg">{{ book.title }}</h1>
-        <p>By: {{ book.author }}</p>
-        <p>published: {{ book.published }}</p>
-      </li>
-    </ul>
+    <p>Form Input Bindings</p>
+
+    <form class="space-y-8">
+      <input
+        type="text"
+        class="border mt-8 px-4 py-1 border-gray-200 outline-0 rounded-md"
+        placeholder="text here"
+        v-model="name" />
+      <br />
+      <textarea
+        name="description"
+        id=""
+        class="border mt-8 px-4 py-1 border-gray-400 outline-0 rounded-md"
+        v-model="descripton"></textarea>
+      <br />
+      <p>Checkbox</p>
+      <div class="flex items-center gap-2">
+        <input
+          type="checkbox"
+          name="agree"
+          v-model="agree" />
+        <label for="agree">Agree for teram and condition</label>
+        {{ agree }}
+      </div>
+      <div>
+        <p>choose team members</p>
+        <div>
+          <input
+            type="checkbox"
+            name="john"
+            value="john"
+            id="john"
+            v-model="teamMembers" />
+          <label for="john">John</label>
+        </div>
+        <div>
+          <input
+            type="checkbox"
+            name="jack"
+            value="jack"
+            id="jack"
+            v-model="teamMembers" />
+          <label for="jack">jack</label>
+        </div>
+
+        {{ teamMembers }}
+      </div>
+      <hr />
+      <div>
+        <p>Picked:</p>
+
+        <div class="flex items-center gap-2">
+          <input
+            type="radio"
+            name=""
+            id="one"
+            value="one"
+            v-model="picked" />
+          <label for="one">One</label>
+        </div>
+        <div class="flex items-center gap-2">
+          <input
+            type="radio"
+            name=""
+            id="two"
+            value="two"
+            v-model="picked" />
+          <label for="two">Two</label>
+        </div>
+        <p>{{ picked }}</p>
+      </div>
+      <hr />
+
+      <div>Selected: {{ selected }}</div>
+
+      <select v-model="selected">
+        <option
+          disabled
+          value="">
+          Please select one
+        </option>
+        <option>A</option>
+        <option>B</option>
+        <option>C</option>
+      </select>
+      <hr />
+
+      <div>
+        <input
+          class="border"
+          type="text"
+          v-model="age" />
+      </div>
+    </form>
   </div>
 </template>
 
-<style>
-input {
-  border: 1px solid #222;
-  border-radius: 10px;
-  padding-right: 50px;
-}
-</style>
-
-
-<!-- home work => Done -->
-<!-- make the list filter by text -->
+<style></style>
